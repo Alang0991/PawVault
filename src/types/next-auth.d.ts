aae96f3926712f630114099775cc279d643e369f
Bluey <alangrant0991@gmail.com>
2026-07-05 12:43:29 +0100

    Add NextAuth role types

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,16 +1,23 @@
 import NextAuth from "next-auth"
+import { DefaultSession } from "next-auth"
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string
-      name?: string | null
-      email?: string | null
-      image?: string | null
-    }
+      role?: string | null
+    } & DefaultSession["user"]
   }
 
   interface User {
     id: string
+    role?: string | null
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id?: string
+    role?: string | null
   }
 }
